c77cff298b14d45438086e4252447b9983bc7dd9
azherid <kwakwasucki@wp.pl>
2023-11-04 15:18:15 +0100

    final

---
+++ config.ts
@@ -0,0 +1,3 @@
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://best-db.vercel.app' : 'http://localhost:3000';
+
+export default API_BASE_URL
--- utils/handlers.ts
+++ utils/handlers.ts
@@ -1,44 +1,42 @@
+import API_BASE_URL from "@/config"
 import axios from "axios"
 
 export async function getFiles(userId:string,collectionId:string) {
-    return axios.get(`http://localhost:3000/api/user/getFiles/${userId}/${collectionId}`)
+    return axios.get(`${API_BASE_URL}/api/user/getFiles/${userId}/${collectionId}`)
 }
 
-
 export async function getCollections(userId:string) {
-    return await axios.get(`http://localhost:3000/api/user/collections/${userId}`)
+    return await axios.get(`${API_BASE_URL}/api/user/collections/${userId}`)
   }
-  
-
   
 export async function getCollection(userId:string,collectionId:string) {
-   return await axios.get(`http://localhost:3000/api/user/collections/${userId}/${collectionId}`)
-    
-  }
+   return await axios.get(`${API_BASE_URL}/api/user/collections/${userId}/${collectionId}`)
+}
   
 export async function findUserById(userId:string){
-    return await axios.get(`http://localhost:3000/api/user/findUserById/${userId}`)
+    return await axios.get(`${API_BASE_URL}/api/user/findUserById/${userId}`)
 }
   
 export async function getAllFiles(userId:string) {
-    return await axios.get(`http://localhost:3000/api/user/getAllFiles/${userId}`)
-   }
+    return await axios.get(`${API_BASE_URL}/api/user/getAllFiles/${userId}`)
+}
 
 export async function getNotifications(userId:string){
-    return await axios.get(`http://localhost:3000/api/user/notifications/getNotifications/${userId}`)
+    return await axios.get(`${API_BASE_URL}/api/user/notifications/getNotifications/${userId}`)
 }
+
 export async function handleNotifications(userId:string,action?:string,notificationId?:string) {
-    return await axios.post(`http://localhost:3000/api/user/notifications/handleNotifications/${userId}`,{action:action,notificationId:notificationId})
+    return await axios.post(`${API_BASE_URL}/api/user/notifications/handleNotifications/${userId}`,{action:action,notificationId:notificationId})
 }
 
 export async function searchResults(userId:string,phrase:string){
-    return await axios.get(`http://localhost:3000/api/user/search/${userId}?phrase=${phrase}`)
+    return await axios.get(`${API_BASE_URL}/api/user/search/${userId}?phrase=${phrase}`)
 }
 
 export async function revokeAcces(ownerId:string,invitedId:string,collectionId:string){
-    return await axios.post(`http://localhost:3000/api/user/collections/revokeAccess/${ownerId}/${collectionId}`,{invitedId:invitedId})
+    return await axios.post(`${API_BASE_URL}/api/user/collections/revokeAccess/${ownerId}/${collectionId}`,{invitedId:invitedId})
 }
    
 export async function deleteCollection(userId:string,collecitonId:string){
-    return await axios.delete(`http://localhost:3000/api/user/collections/${userId}/${collecitonId}`)
+    return await axios.delete(`${API_BASE_URL}/api/user/collections/${userId}/${collecitonId}`)
 }
